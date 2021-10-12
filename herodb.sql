7121402f8e4a17d9d5d93eaa7a6b27f7f73ea461
PedroHenrique109 <pedro.ribeiro109@etec.sp.gov.br>
2021-10-12 19:28:31 -0300

    instalação do json-server

---
+++ herodb.sql
@@ -0,0 +1,26 @@
+
+create database android;
+
+use android;
+
+CREATE TABLE `heroes` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `realname` varchar(200) NOT NULL,
+  `rating` int NOT NULL,
+  `teamaffiliation` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `heroes` (`id`, `name`, `realname`, `rating`, `teamaffiliation`)
+VALUES (1, 'Pantera Negra', 'Viúva-Negra', 3, 'Avengers'),
+(2, 'Ciborgue', 'Aquaman', 5, 'Justice League');
+
+ALTER TABLE `heroes`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `heroes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+
---
+++ includes/Constants.php
@@ -0,0 +1,7 @@
+<?php 
+
+
+	define('DB_HOST', '192.168.0.115');
+	define('DB_USER', 'etecia');
+	define('DB_PASS', '123456');
+	define('DB_NAME', 'android');
